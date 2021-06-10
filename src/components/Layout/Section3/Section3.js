2ffed627f4318f51e1e55b5a81fb8d059439100b
Your Name <you@example.com>
2021-06-10 15:36:57 +0600

    section3

---
+++ src/components/Layout/Section3/Section3.js
@@ -0,0 +1,29 @@
+import classes from "./Section3.module.css";
+import img1 from "../../../images/section33.jpg";
+
+const Section3 = () => {
+  return (
+      <div id="About" className={classes.img} style={{ backgroundImage: `url(${img1})` }}>
+        <div className={classes.Section3contaner}>
+          <div className={classes.rooter}>
+            <h4>О компьютерном клубе</h4>
+            <h2 >OUR MISSION</h2>
+            <p>
+              YouPlay – это уютный компьютерный клуб, оборудованный всем
+              необходимым для качественной и атмосферной онлайн-игры. К нам в
+              киберклуб приходят ценители активного киберспортивного и
+              виртуального отдыха, а также для того, чтоб найти
+              единомышленников. Благодаря производительным компьютером вы можете
+              поиграть в любую новинку или состязаться с мощным
+              онлайн-противником без подвисания компьютера и «глюков».
+            </p>
+             <div className={classes.button}>
+             <a>забронировать<br/>+7(977)1478842</a>
+             </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Section3;
