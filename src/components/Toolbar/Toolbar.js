5462c6ecab8fe975e43610e7d34ded5ff6538e3a
Your Name <you@example.com>
2021-06-10 15:43:08 +0600

    toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,36 @@
+import classes from "./Toolbar.module.css";
+import logo from "../../images/venum.png";
+
+const Toolbar = () => {
+  return (
+    <div className={classes.Toolbar}>
+      <div className={classes.Toolbarcontainer}>
+        <div className={classes.logo}>
+          <img src={logo} alt="Logo of the Music" />
+        </div>
+
+        <div className={classes.nav}>
+          <ul>
+            <li>
+              <a href="#About">О нас</a>
+            </li>
+            <li>
+              <a href="#jelezo" >Железо</a>
+            </li>
+            <li>
+              <a href="#price">Цены</a>
+            </li>
+            <li>
+              <a href="#games">Игры</a>
+            </li>
+            <li>
+              <a href="#contact">Контакты клуба</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Toolbar;
