34867879a1a1d2748b17efc3656eebf29f875784
Your Name <you@example.com>
2021-06-10 23:21:37 +0600

    added link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,14 +18,14 @@ const Footer = () => {
           <h6>LOCATION</h6>
           <ul className={classes.footerdjjiv}>
               
-              <li><img src={map} alt="Logo of the Music"/><a> Ленком Майка, Кафе "Дениз" 2 этаж</a></li>
+              <li><img src={map} alt="Logo of the Music"/><a  href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x38865be97576c22f:0xb61d323ddff08b41?utm_source=mstt_1&entry=gps"> Ленком Майка, Кафе "Дениз" 2 этаж</a></li>
               <img src={star} alt="Logo of the Music" style={{ width: ` 150px`, height: ` 50px`}}/>
           </ul>
       </div>
       <div className={classes.footerdjjiv}>
           <h6>CONNECT</h6>
-          <p><img src={phone} alt="Logo of the Music"/><a>+996700001681</a></p>
-          <p><a><img src={inst} alt="Logo of the Music"/></a></p>
+          <p><img src={phone} alt="Logo of the Music"/><a href="tel:+996700001681">+996700001681</a></p>
+          <p><a href="https://www.instagram.com/"><img src={inst} alt="Logo of the Music"/></a></p>
       </div>
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,9 +10,9 @@ const Header = () => {
         <div className={classes.Headercontainer}>
           <div className={classes.leftparth}>
               <ul>
-              <li><img src={phone} alt="Logo of the Music"/><a>+996700001681</a></li>
-              <li><img src={map} alt="Logo of the Music"/><a> Кафе "Дениз" 2 этаж</a></li>
-              <li><a><img src={inst} alt="Logo of the Music"/></a></li>
+              <li><img src={phone} alt="Logo of the Music"/><a  href="tel:+996700001681">+996700001681</a></li>
+              <li><img src={map} alt="Logo of the Music"/><a href="https://www.google.com/maps/place//data=!4m2!3m1!1s0x38865be97576c22f:0xb61d323ddff08b41?utm_source=mstt_1&entry=gps"> Кафе "Дениз" 2 этаж</a></li>
+              <li><a href="https://www.instagram.com/"><img src={inst} alt="Logo of the Music"/></a></li>
               </ul>
           </div>
           <div className={classes.ridhtparth}>
