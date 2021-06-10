1c8b61304b808bf0867249ffcd1e5dc554ec6a13
Your Name <you@example.com>
2021-06-10 15:35:59 +0600

    section 2

---
+++ src/components/Layout/Section2/Section2.js
@@ -0,0 +1,38 @@
+import classes from "./Section2.module.css";
+import img21 from "../../../images/section2-1.jpg";
+import img22 from "../../../images/section2-2.jpg";
+import img23 from "../../../images/section2-3.jpg";
+
+const Section2 = () => {
+  return (
+    <div className={classes.Section2}>
+        <div className={classes.section}>
+
+          <div  className={classes.img21} style={{ backgroundImage: `url(${img21})` }}>
+            <div className={classes.section2div}>
+
+               <h2>ТОП ЖЕЛЕЗО</h2>
+              <h4>22 компьютера - Основной зал / Vip зона</h4>
+            </div>
+          </div>
+
+          <div  className={classes.img22} style={{ backgroundImage: `url(${img22})` }}>
+          <div className={classes.section2div}>
+              <h2>Open -24/7</h2>
+              <h4>Работает для вас без перерыва копицентр</h4>
+            </div>
+          </div>
+
+          <div  className={classes.img23} style={{ backgroundImage: `url(${img23})` }}>
+          <div className={classes.section2div}>
+              <h2>AFK LOCATION</h2>
+              <h4>Кофе на вынос / Коворкинг / Chill зона</h4>
+            </div>
+          </div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Section2;
