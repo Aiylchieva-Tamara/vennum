7d2aeb892df11950e39048fc2c26af3740d0c81b
Your Name <you@example.com>
2021-06-10 22:50:46 +0600

    fixed error

--- src/components/Layout/Section5/Section5.js
+++ src/components/Layout/Section5/Section5.js
@@ -1,9 +1,6 @@
 import classes from "./Section5.module.css";
 import img1 from "../../../images/section5.jpg";
 import Club from "./Club/Club";
-import Cafe from "./Cafe/Cafe";
-import Copy from "./Copy/Copy";
-
 const Section5 = () => {
   return (
     <div id="price" className={classes.img} style={{ backgroundImage: `url(${img1})`}}>
@@ -11,12 +8,7 @@ const Section5 = () => {
      <div>
         <Club />
       </div>
-      <div>
-        <Cafe />
-      </div>
-      <div>
-        <Copy />
-      </div>
+     
      </div>
     </div>
   );
