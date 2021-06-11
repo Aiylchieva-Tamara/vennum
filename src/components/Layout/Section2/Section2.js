13e9522e6cb7aad45f75e8059a699cfd68e050fd
Your Name <you@example.com>
2021-06-11 10:14:37 +0600

    delite section

--- src/components/Layout/Section2/Section2.js
+++ src/components/Layout/Section2/Section2.js
@@ -1,7 +1,6 @@
 import classes from "./Section2.module.css";
 import img21 from "../../../images/section2-1.jpg";
 import img22 from "../../../images/section2-2.jpg";
-import img23 from "../../../images/section2-3.jpg";
 
 const Section2 = () => {
   return (
@@ -19,16 +18,11 @@ const Section2 = () => {
           <div  className={classes.img22} style={{ backgroundImage: `url(${img22})` }}>
           <div className={classes.section2div}>
               <h2>Open -24/7</h2>
-              <h4>Работает для вас без перерыва копицентр</h4>
+              <h4>Работает для вас без перерыва</h4>
             </div>
           </div>
 
-          <div  className={classes.img23} style={{ backgroundImage: `url(${img23})` }}>
-          <div className={classes.section2div}>
-              <h2>AFK LOCATION</h2>
-              <h4>Кофе на вынос / Коворкинг / Chill зона</h4>
-            </div>
-          </div>
+         
         </div>
       
     </div>
